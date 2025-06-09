263c688cd5394845f542d0e650a190ea07cf5802
Britto132 <01fe23bca132@kletech.ac.in>
2025-06-09 09:32:45 +0530

    Save this file

---
+++ Positive.java
@@ -0,0 +1,13 @@
+public class Positive{
+    public static void main(String[] args) {
+        int number=7;
+        if (number > 0) {
+            System.out.println("The Number is Positive");
+        }
+        else{
+            System.out.println("The Number is Not Positive");
+        }
+        
+            
+        }
+}
